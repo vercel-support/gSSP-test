c5ebe8f1a5f32847016f50e7e9dc9b325cf34a14
Riz <rizwana.akmal@hotmail.com>
2023-03-31 15:43:51 +0100

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export const getServerSideProps = (context) => {
+  // const { data, errors } = await shopifyFetch({
+  //     query: productsList
+  // });
+
+  return {
+    props: {
+      message: "TEST",
+    },
+  };
+};
+
+export default function Home({ message }) {
   return (
     <>
       <Head>
@@ -18,7 +30,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>pages/index.js {message}</code>
           </p>
           <div>
             <a
@@ -26,7 +38,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +131,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
